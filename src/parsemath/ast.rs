d1173986f41e03e4137279233bc51e1f2b8bf58d
lsacienne <alexandre.viala@outlook.com>
2024-05-06 19:29:40 -0400

    ✨ Implement AST construction

--- src/parsemath/ast.rs
+++ src/parsemath/ast.rs
@@ -1,3 +1,5 @@
+use std::error;
+
 #[derive(Debug, Clone)]
 pub enum Node {
     Add(Box<Node>, Box<Node>),
@@ -7,4 +9,17 @@ pub enum Node {
     Caret(Box<Node>, Box<Node>),
     Negative(Box<Node>),
     Num(f64),
+}
+
+pub fn eval(expr: Node) -> Result<f64, Box<dyn error::Error>> {
+    use self::Node::*;
+    match expr {
+        Num(i) => Ok(i),
+        Add(expr1, expr2) => Ok(eval(*expr1)? + eval(*expr2)?),
+        Substract(expr1, expr2) => Ok(eval(*expr1)? - eval(*expr2)?),
+        Multiply(expr1, expr2) => Ok(eval(*expr1)? * eval(*expr2)?),
+        Divide(expr1, expr2) => Ok(eval(*expr1)? / eval(*expr2)?),
+        Negative(expr1) => Ok(- (eval(*expr1)?)),
+        Caret(expr1, expr2) => Ok(eval(*expr1)?.powf(eval(*expr2)?))   
+    }
 }
