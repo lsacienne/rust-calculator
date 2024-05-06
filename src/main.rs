5b6950969f08e91dc0cc97b09942b60d90519d05
lsacienne <alexandre.viala@outlook.com>
2024-05-06 19:30:27 -0400

    🔀 Merge UI with parser

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
+use parsemath::parser::{Parser, ParseError};
+use slint::SharedString;
+
+use crate::parsemath::ast;
+
 slint::include_modules!();
+mod parsemath;
 
 fn main() -> Result<(), slint::PlatformError> {
-    use slint::Model;
-
     let ui = AppWindow::new()?;
+    let ui_weak = ui.as_weak();
+    ui.on_validate(move || {
+        let ui = ui_weak.unwrap();
+        let expression_ui: String = ui.get_expression().to_string();
+        match evaluate(expression_ui.clone()) {
+            Ok(result) => {
+                ui.set_result(SharedString::from(result.to_string()));
+                ui.set_is_err(false);
+            },
+            Err(e) => {
+                ui.set_result(SharedString::from(e.to_string()));
+                ui.set_is_err(true);
+            }
+        }
+    });
     ui.run()
+}
+
+fn evaluate(expr: String) -> Result<f64, ParseError> {
+    let expr = expr.split_whitespace().collect::<String>();
+    // remove whitespace chars
+    let mut math_parser = Parser::new(&expr)?;
+    let ast = math_parser.parse()?;
+    println!("The generated AST is {:?}", ast);
+    Ok(ast::eval(ast)?)
 }
